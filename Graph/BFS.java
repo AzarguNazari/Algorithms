8c18915a1c62c0f5851be567ceabf1f468b1744a
Hazar Gul Nazari <nazariazargul@gmail.com>
2019-04-02 03:26:30 -0700

    Folder Changes

---
+++ Graph/BFS.java
@@ -0,0 +1,22 @@
+// implementation of BFS though array
+public static void BFS(ArrayList<String> vertices, int[][] edges, String from, String to) {
+        boolean[] visited = new boolean[edges.length];
+        int index = vertices.indexOf(from);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(index);
+        visited[index] = true;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(vertices.get(node) + " ");
+            if (vertices.get(node).equals(to)) {
+                return;
+            }
+            for (int x = 0; x < edges.length; x++) {
+                if (edges[node][x] > 0 && !visited[x]) {
+                    queue.add(x);
+                    visited[x] = true;
+
+                }
+            }
+        }
+    }
