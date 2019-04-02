dd27c7fe1679dc668fdecfc6bab16f05a8cbc9f8
Hazar Gul Nazari <nazariazargul@gmail.com>
2019-04-02 04:05:01 -0700

    Tree Added

---
+++ Tree/Node.java
@@ -0,0 +1,36 @@
+package LinkedListGraph;
+
+import java.util.LinkedList;
+
+/**
+	* 
+	*	@author Hazargul Nazari
+	*/
+
+public class Node {
+
+    private String name;
+    private LinkedList<String> connected;
+    
+    public Node(String name){
+        this.name = name;
+        connected = new LinkedList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LinkedList<String> getConnected() {
+        return connected;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString(){
+        return this.name + " -> " + this.connected;
+    }
+}
